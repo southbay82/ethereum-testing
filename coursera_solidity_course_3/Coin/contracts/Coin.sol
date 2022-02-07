a32dfb97bfb739ebbdfe7df0899122c058ae3b27
Ryan Behan <southbay82@yahoo.com>
2022-02-07 13:49:06 -0500

    Squashed commit of the following:
    commit 098d8574ab689ea396dbd8c027a12faa558db708
    Author: southbay82 <southbay82@gmail.com>
    Date:   Sun Jan 2 19:15:59 2022 -0500
    
        Updated web3, truffle/contract and code to modern standards for this code to function with a modern version of Metamask.
    
    commit ca172bc422d3466f66a3f7f89f6e35e1222cdbb3
    Author: southbay82 <southbay82@gmail.com>
    Date:   Thu Dec 30 21:43:46 2021 -0500
    
        temp commit, researching the deprecation error on app load in browser.
    
    commit 94042ecf561eef3fa5850633fbf4f13ccbc8f20c
    Author: southbay82 <southbay82@gmail.com>
    Date:   Tue Dec 28 22:11:54 2021 -0500
    
        reverted change to contract reference, for some reason vote is the contract name to use. Will need to dig on this later.
    
    commit b4a08628e14e084e6a2aefaa3f1625141a0bf057
    Author: Ryan Behan <you@example.com>
    Date:   Tue Dec 28 21:57:20 2021 -0500
    
        file management, continued work on app.js. app is not fully functional likely due to deprecated code.
    
    commit cf07b21f1c45eedcc6d865e0d5b933ac67abfdbf
    Author: Ryan Behan <you@example.com>
    Date:   Tue Dec 28 21:55:52 2021 -0500
    
        minor updates, app is still not fully functional
    
    commit 39eb6d0242238a88ee3e89be952e6a4157daeaf1
    Author: Ryan Behan <you@example.com>
    Date:   Sat Nov 27 06:05:46 2021 -0500
    
        Coin project now compiles with new compiler version. Minor syntax updates.
    
    commit ee61fc69558e85c79ef17aee4be2ee38e084d339
    Author: Ryan Behan <you@example.com>
    Date:   Sat Nov 27 05:50:20 2021 -0500
    
        directory renaming for consistency.
    
    commit 4295173517b3c2d60e13e16f546d739f7a8525a1
    Author: Ryan Behan <you@example.com>
    Date:   Sat Nov 27 05:48:23 2021 -0500
    
        Initial commit for course 3, no changes to code.
    
    commit 95d1336f573bbf4cb90cbef082bcdde961101095
    Author: Ryan Behan <you@example.com>
    Date:   Sat Sep 18 20:44:17 2021 -0400
    
        coures completed 100% score
    
    commit 7d6b1d99bfec48526385c272f3082b092cd7e731
    Author: Ryan Behan <you@example.com>
    Date:   Sun Aug 29 22:11:57 2021 -0400
    
        Synced changes from Remix project so we can load this repo from Remix.
    
    commit 5bfd71764f085ff9ac6d470648668baa21c8a79a
    Author: RYAN BEHAN <ryanbehan@skynet.local>
    Date:   Sun Aug 29 22:00:42 2021 -0400
    
        All contracts compile now.
    
    commit 312515c9d6b18499d82716943a5dca72b864b89f
    Author: RYAN BEHAN <ryanbehan@skynet.local>
    Date:   Sun Aug 29 20:59:12 2021 -0400
    
        Created truffle directories to correct these source files.

---
+++ coursera_solidity_course_3/Coin/contracts/Coin.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.16;
+contract Coin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    constructor() public {
+        minter = msg.sender;
+        balances[msg.sender] += 11;
+    }
+
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter)
+          revert();
+        balances[receiver] += amount;
+    }
+
+    function burn(address receiver, uint amount) public {
+        if (msg.sender != minter)
+          revert();
+        balances[receiver] -= amount;
+    }
+
+    function transfer(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount)
+          revert();
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
